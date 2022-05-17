e7e012692d9e0df302c26bfef62f01826a126fab
LeoVellozo <105672545+LeoVellozo@users.noreply.github.com>
2022-05-17 19:16:29 -0300

    [UPLOAD] FILES UP [UPLOAD]

---
+++ js/function.js
@@ -0,0 +1,18 @@
+function irTop() {
+    $("html, body").animate({ scrollTop: 0 });
+}
+
+function irCriacoes() {
+    $("html, body").animate({ scrollTop: 450 });
+
+}
+
+function irSobreMim() {
+    $("html, body").animate({ scrollTop: 900 });
+
+}
+
+function irContate() {
+    $("html, body").animate({ scrollTop: 1500 });
+
+}
